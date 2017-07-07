d2846654bb0ece589e5405cfca8a5df50995e3c0
Administrator <Administrator@192.168.81.135>
2017-07-07 20:13:47 +0900

    a

--- www/js/data_event.js
+++ www/js/data_event.js
@@ -1,6 +1,43 @@
+var calculator = {
+	planFee : {a:100000, b : 12000, c : 8800, d : 5500, f : 9900},
+	smartPhone : {a : 999900, b : 123000, c : 100000, d : 12 , e : 0},
+	smartPhone_levelpayment_value : function (){
+		var interestRates;
+		var totalValue;
+		
+		if(this.smartPhone.e == 0){
+		/*sk*/
+		interestRates = 5.9;
+			
+		}else if (this.smartPhone.e == 1){
+		/*kt*/
+		}else if (this.smartPhone.e == 2){
+		/*lg*/
+		}else if (this.smartPhone.e == 3){
+		/*cj*/
+		}else if (this.smartPhone.e == 4){
+		/*기타*/
+		}else if (this.smartPhone.e == 5){
+		/*기타1*/
+		}else if (this.smartPhone.e == 6){
+		/*기타2*/
+		}
+		return interestRates;
+	},
+	planvalue : null,
+	smartPhoneValue : null,
+	method_1 : function () {
+
+		this.planvalue = this.planFee.a + this.planFee.f - this.planFee.b - this.planFee.c - this.planFee.d;
+		this.smartPhoneValue = this.smartPhone_levelpayment_value();
+		
+		alert(this.planvalue + " : "+this.smartPhoneValue)
+	} 
+};
 
 function v1_div1_Disclosure_event () {
 	alert("공시지원")
+	calculator.method_1();
 };
 function v1_div1_Choice_event () {
 	alert("선택약정")
@@ -8,4 +45,5 @@ function v1_div1_Choice_event () {
 function v1_1_Installment_event (pram) {
 	alert("할부"+pram+"개월")
 };
+
 
--- www/js/tag_event.js
+++ www/js/tag_event.js
@@ -2,19 +2,23 @@ $(function(){
 	var  choice = $("#v1_div_choice button");
 	
 	choice.click(function(){
-		var val = $(this).val();
 		
-		if(val == 1){
+		if($(this).index() == 0){
+			
 			v1_div1_Disclosure_event();
+			
 		}else{
+			
 			v1_div1_Choice_event();
+			
 		}
-		alert("abbb")
+		
 	})
 	
 	var installment = $("#v1_div_Installment button")
 	
 	installment.click(function(){
+		
 		var val = $(this).html();
 		
 		v1_1_Installment_event (val);
