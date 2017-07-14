2d33db40c77cac43c9ebbde8d319a0e520c78a99
necromman <necromman@gmail.com>
2017-07-14 09:18:01 +0900

    이대훈 커밋 푸쉬

--- www/js/main-sock.js
+++ www/js/main-sock.js
@@ -16,26 +16,26 @@ $(function () {
     var d;
 
     function deviceAnime() {
-        device = TweenLite.to(deviwrap, 1.1, {top: 0,ease: Quart.easeInOut});
+        device = TweenLite.to(deviwrap, 1.1, {top: "0%",ease: Quart.easeInOut});
         b = TweenLite.to(secondcol, 1, {right: "-58.5%",ease: Quart.easeInOut});
         c = TweenLite.to(thirdcol, 1, {left: "-8.2%",ease: Quart.easeInOut});
         d = TweenLite.to(fourthcol, 1, {right: "-25%",ease: Quart.easeInOut});
     }
     function acDeviceAnime() {
-        device = TweenLite.to(deviwrap, 1.1, {top: 0,ease: Quart.easeInOut});
+        device = TweenLite.to(deviwrap, 1.1, {top: "0%",ease: Quart.easeInOut});
         b = TweenLite.to(secondcol, 1, {right: "-58.5%",ease: Quart.easeInOut});
         c = TweenLite.to(thirdcol, 1, {left: "-8.2%",y:0,ease: Quart.easeInOut});
         d = TweenLite.to(fourthcol, 1, {right: "-25%",ease: Quart.easeInOut});
     }
 
     // function planAnime() {
-    //     plan = TweenLite.to(planwrap, 1.1, {top: 0,ease: Quart.easeInOut});
+    //     plan = TweenLite.to(planwrap, 1.1, {top: "0%,ease: Quart.easeInOut});
     //     b = TweenLite.to(secondcol, 1, {right: "-58.5%",ease: Quart.easeInOut});
     //     c = TweenLite.to(thirdcol, 1, {left: "-8.2%",ease: Quart.easeInOut});
     //     d = TweenLite.to(fourthcol, 1, {right: "-25%",ease: Quart.easeInOut});
     // }
     function planAnime() {
-        plan = TweenLite.to(planwrap, 1.1, {top: 0,ease: Quart.easeInOut});
+        plan = TweenLite.to(planwrap, 1.1, {top: "0%",ease: Quart.easeInOut});
         b = TweenLite.to(secondcol, 1, {right: "-33.3%",ease: Quart.easeInOut});
         c = TweenLite.to(thirdcol, 1, {y:1000,ease: Quart.easeInOut});
         d = TweenLite.to(fourthcol, 1, {right: "0%",ease: Quart.easeInOut});
